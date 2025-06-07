c08d875d898fa3f7e90233a1067f4d6c5219f9e1
Balunaf <leo.chatelet@sfr.fr>
2025-06-07 18:23:47 +0200

    Main function

--- tondeuse/src/main/java/pro/kata/MowItNow.java
+++ tondeuse/src/main/java/pro/kata/MowItNow.java
@@ -1,7 +1,23 @@
 package pro.kata;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MowItNow {
     public static void main(String[] args) {
-        System.out.println("Tests only for now!");
+        if(args.length == 0)
+            System.out.println("Lacking a file argument");
+        else{
+            try{
+                List<MowingState> mowingStates = InstructionDecryptor.readFile(args[0]);
+                for (MowingState state : mowingStates){
+                    state.run();
+                }
+                mowingStates.stream().forEach(state -> {System.out.print(state.toString()+" ");});
+                System.out.println();
+            } catch (IOException e){
+                System.out.println("File name '"+args[0]+" is not recognized");
+            }
+        }
     }
 }
